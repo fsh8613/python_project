f2968a1285dd0e0e87332d0a5560fcdbb2b518f9
fsh <fdhoseinee@gmail.com>
2026-07-22 11:54:13 +0330

    add new defs

--- StudentManager.py
+++ StudentManager.py
@@ -9,4 +9,39 @@ class StudentManager:
             if s.student_id == student.student_id:
                 raise ValueError("Student ID already exists!")
 
-        self.students.append(student)
+            self.students.append(student)
+             
+    def calculate_average_gpa(self):
+
+        if len(self.students) == 0:
+            return 0
+
+        total = 0
+
+        for student in self.students:
+            total += student.calculate_gpa()
+
+        return total / len(self.students)
+    
+    def get_best_student(self):
+        return max(
+        self.students,
+        key=lambda student: student.calculate_gpa()
+         )
+        
+    def get_course_average(self, course):
+
+      total = 0
+      count = 0
+
+      for student in self.students:
+
+        if course in student.grades:
+            total += student.grades[course]
+            count += 1
+
+        if count == 0:
+            print("this course is not exist!")
+            return 0
+
+        return total / count
